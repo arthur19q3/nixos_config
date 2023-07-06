31793843542bc65575f1e484ed1d9024d7c4e97f
arthur19q3 <theartoforz@gmail.com>
2023-07-06 12:49:40 +0800

    init

--- home.nix
+++ home.nix
@@ -1,17 +1,33 @@
+# home.nix
+
 { config, pkgs, ... }:
 
 {
   imports = [
+    <home-manager/nixos>
   ];
 
+  home.username = "arthur19q3";
+  home.homeDirectory = "/home/arthur19q3";
+  home.stateVersion = "23.05";
 
-  home = {
-    username = "arthur19q3";
-    homeDirectory = "/home/arthur19q3";
-    stateVersion = "23.05";
+  programs.home-manager.enable = true;
+
+  # Declare Cargo package in home.packages
+  home.packages = [
+    pkgs.cargo
+    # Add other packages if needed
+  ];
+
+  # Configure Cargo if necessary
+  programs.cargo = {
+    enable = true;
+    # Add specific configuration options for Cargo here
   };
 
-  programs = {
-    home-manager.enable = true;
+  # Other configurations...
+  home-manager.users.arthur19q3.environment={
+    #
   };
 }
+
