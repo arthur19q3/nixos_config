615437fa5878069ff1685dec12f843250f97a0ed
arthur19q3 <theartoforz@gmail.com>
2023-07-08 05:09:06 +0800

    init

--- configuration.nix
+++ configuration.nix
@@ -100,8 +100,6 @@ in
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-
-
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
